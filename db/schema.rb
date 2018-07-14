e05be498c1920da28e3ce00a0f816f652474a7a2
Subin Choi <subin_97@naver.com>
2018-07-14 17:49:24 +0000

    pulllll

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180714161957) do
+ActiveRecord::Schema.define(version: 20180714163643) do
 
   create_table "dong_aris", force: :cascade do |t|
     t.string   "name"
@@ -20,8 +20,6 @@ ActiveRecord::Schema.define(version: 20180714161957) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-ActiveRecord::Schema.define(version: 20180714163643) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -41,5 +39,4 @@ ActiveRecord::Schema.define(version: 20180714163643) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
->>>>>>> 216084a97258a43a4757a44e8efbcc7aa7394c8b
 end
