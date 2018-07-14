dbae1949b2c1b20daa8bd120e34c55c91382bec0
Subin Choi <subin_97@naver.com>
2018-07-14 22:05:37 +0000

    집에 가자

--- db/migrate/20180714163643_devise_create_users.rb
+++ db/migrate/20180714163643_devise_create_users.rb
@@ -6,7 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :name
+      t.string :nickname
+      t.string :bday
+      t.string :gender
+      t.string :phone_number
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
